3bfc7c4438b38db1555dc58a31ccea94729e1df5
Rahul <7678478061k@gmail.com>
2024-06-25 12:42:08 +0530

    style: change blob-animation speed and intro-panel width on 2xl screens

--- app/components/home/intro-panel.tsx
+++ app/components/home/intro-panel.tsx
@@ -3,12 +3,12 @@ import TypeWriterText from "./type-writer-text";
 
 export default function IntroPanel() {
   return (
-    <div className="relative w-full select-none space-y-6 xl:w-1/2 2xl:w-5/12 2xl:space-y-8">
+    <div className="relative w-full select-none space-y-6 xl:w-1/2 2xl:w-6/12 2xl:space-y-8">
       <h2 className="text-5xl font-bold text-main sm:text-6xl md:text-7xl">
         Rahul
       </h2>
       <TypeWriterText />
-      <p className="font-secondary text-base tracking-tight text-primary-foreground sm:text-xl md:text-2xl">
+      <p className="font-secondary text-base tracking-tight text-primary-foreground sm:text-xl md:text-2xl 2xl:mr-10">
         Passionate self-taught developer skilled in problem-solving, quick at
         finding the best solutions online, and dedicated to continuous learning
         and impactful projects.
