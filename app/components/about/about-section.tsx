2f70edddce18e9dc30826d8118629c4d1ae92692
Rahul <7678478061k@gmail.com>
2024-07-07 20:23:53 +0530

    style: add padding to sections

--- app/components/about/about-section.tsx
+++ app/components/about/about-section.tsx
@@ -5,13 +5,12 @@ import AboutText from "./about-text";
 export default function AboutSection() {
   return (
     <section id="about">
-      <div className="flex flex-col space-y-6 py-8 lg:px-20">
+      <div className="flex flex-col space-y-6 py-8 lg:px-20 2xl:px-72">
         <SectionHeading text="Who Am I" />
 
         <AboutText />
 
         <AboutExp />
-        
       </div>
     </section>
   );
--- app/components/projects/projects-section.tsx
+++ app/components/projects/projects-section.tsx
@@ -4,7 +4,7 @@ import Projects from "./projects";
 export default function ProjectsSection() {
   return (
     <section id="projects">
-      <div className="flex flex-col space-y-6 py-8">
+      <div className="flex flex-col space-y-6 py-8 2xl:px-24">
         <div>
           <SectionHeading text="Projects" />
         </div>
