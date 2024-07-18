1e91d6c0173ace815c33e9347909e880580391ab
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-07-18 16:56:17 +0000

    Fix issue #2

--- app/components/header/nav-bar.tsx
+++ app/components/header/nav-bar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import styles from './NavBar.module.css'; // Adjust the path as necessary
 
 export default function NavBar() {
   return (
@@ -6,27 +7,27 @@ export default function NavBar() {
       <ul className="z-10 gap-14 text-xl text-secondary-foreground hidden md:flex">
         <li>
           <Link href="/">
-            <span className="hover:text-violet-500 hover:text-2xl cursor-pointer">Home</span>
+            <span className={styles.navLink}>Home</span>
           </Link>
         </li>
         <li>
           <Link href="#about">
-            <span className="hover:text-violet-500 hover:text-2xl cursor-pointer">About</span>
+            <span className={styles.navLink}>About</span>
           </Link>
         </li>
         <li>
           <Link href="#skills">
-            <span className="hover:text-violet-500 hover:text-2xl cursor-pointer">Skills</span>
+            <span className={styles.navLink}>Skills</span>
           </Link>
         </li>
         <li>
           <Link href="#projects">
-            <span className="hover:text-violet-500 hover:text-2xl cursor-pointer">Projects</span>
+            <span className={styles.navLink}>Projects</span>
           </Link>
         </li>
         <li>
           <Link href="#contact">
-            <span className="hover:text-violet-500 hover:text-2xl cursor-pointer">Contact</span>
+            <span className={styles.navLink}>Contact</span>
           </Link>
         </li>
       </ul>
