9998780d622e838dcf69193bcb50eeb2b8b79f86
Vaibhav Modi <123352253+vaibhavmodi45@users.noreply.github.com>
2024-08-01 22:35:03 +0530

    Remove bar-icon.tsx
    The navbar icon, which has no need is removed in this.

--- app/components/header/bar-icon.tsx
+++ app/components/header/bar-icon.tsx
@@ -1,9 +1,1 @@
-import Image from "next/image";
 
-export default function BarIcon() {
-  return (
-    <div className="cursor-pointer md:hidden">
-      <Image src="/svg/bar.svg" width={30} height={30} alt="bar-icon" />
-    </div>
-  );
-}
